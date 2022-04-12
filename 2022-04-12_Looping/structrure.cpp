a654bf617ee7432a4c866661a28b177c45486700
tianreformis <tianreformis@gmail.com>
2022-04-12 08:38:33 +0800

    materi struktur

---
+++ 2022-04-12_Looping/structrure.cpp
@@ -0,0 +1,14 @@
+//header
+#include <iostream>
+#include <stdio.h>
+
+//standard space
+using namespace std;
+
+//main function
+int main() { //begin of function
+    cout<<"Kristian Reformis";
+    
+    //cout untuk menampilkan
+    return 0;
+}//end of function
