56ef4ab22dbb0226e6dc0d02a40d1418da5c642f
tianreformis <tianreformis@gmail.com>
2022-04-12 11:08:17 +0800

    Tambahkan IO2

--- 2022-04-12_Input_Output/input_output2.cpp
+++ 2022-04-12_Input_Output/input_output2.cpp
@@ -4,9 +4,14 @@
 using namespace std;
 int main() { 
     string nama;
+    int umur, kelas, tahun_lahir;
     cout<<"Masukkan Nama = ";cin>>nama;
-    cout<<"Nama Anda = "<<nama;
-    
-    int umur, kelas;
+    cout<<"Masukkan Kelas = ";cin>>kelas;
+    cout<<"Masukkan Tahun lahir = ";cin>>tahun_lahir;
+    umur = 2022 - tahun_lahir;
 
+    cout<<"Nama Anda = "<<nama<<endl;
+    cout<<"Kelas Anda = "<<kelas<<endl;
+    cout<<"Umur Anda = "<<umur<<" Tahun"<<endl; 
+    return 0;
 }
