cf56cb4879dde597013e17a2540b84a81c884c9a
tianreformis <tianreformis@gmail.com>
2022-04-12 11:26:07 +0800

    Tambahkan IO2

--- 2022-04-12_Input_Output/input_output2.cpp
+++ 2022-04-12_Input_Output/input_output2.cpp
@@ -3,15 +3,28 @@
 
 using namespace std;
 int main() { 
-    string nama;
-    int umur, kelas, tahun_lahir;
+    string nama,keterangan;
+    int umur, kelas, tahun_lahir, jk;
     cout<<"Masukkan Nama = ";cin>>nama;
     cout<<"Masukkan Kelas = ";cin>>kelas;
     cout<<"Masukkan Tahun lahir = ";cin>>tahun_lahir;
     umur = 2022 - tahun_lahir;
+    cout<<"Pilih Jenis kelamin = [1]LK [2] PR =";cin>>jk;
+
+    if (jk == 1)  {
+        keterangan = "Laki - Laki";
+    }
+    else if (jk == 2){
+        keterangan = "Perempuan";
+    }
+    else {
+        keterangan = "Jenis kelamin tidak diketahui";
+    }
+
 
     cout<<"Nama Anda = "<<nama<<endl;
     cout<<"Kelas Anda = "<<kelas<<endl;
     cout<<"Umur Anda = "<<umur<<" Tahun"<<endl; 
+    cout<<"Jenis Kelamin = "<<keterangan<<endl; 
     return 0;
 }
