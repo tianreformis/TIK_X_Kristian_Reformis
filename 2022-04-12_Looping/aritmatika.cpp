d86511f303154e4089c8544be91b0460969d1e8a
tianreformis <tianreformis@gmail.com>
2022-04-12 09:16:07 +0800

    Tambahkan aritmatika

---
+++ 2022-04-12_Looping/aritmatika.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() { 
+    int nilai_a, nilai_b;
+    cout<<"Masukkan angka  pertama = ";cin>>nilai_a;
+    cout<<"Masukkan angka  kedua = ";cin>>nilai_b;
+
+    int hasilbagi, hasilkali, hasiltambah, hasilkurang;
+    hasilkali = nilai_a * nilai_b;
+    hasilbagi = nilai_a / nilai_b;
+    hasiltambah = nilai_a + nilai_b;
+    hasilkurang = nilai_a - nilai_b;
+
+}//end of function
---
+++ 2022-04-12_Looping/input_output.cpp
@@ -0,0 +1,20 @@
+//header
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+//standard space
+using namespace std;
+
+//main function
+int main() { //begin of function
+    string nama;
+    int umur;
+    cout<<"Masukkan Nama Anda = ";cin>>nama;
+    cout<<"Masukkan Umur Anda = ";cin>>umur;
+    cout<<"Nama yang dimasukkan = "<<nama<<endl;
+    cout<<"Umur yang dimasukkan = "<<umur<<endl;
+
+
+    return 0;
+}//end of function
