0dfda3e37a6f40f3dbeb8e39d07fdeeb4da83a38
tianreformis <tianreformis@gmail.com>
2022-04-14 11:29:49 +0800

    add if

---
+++ 2022-04-1_Input_Output_X_MIA/input_output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    /* 
+    cin input nama
+    cin input umur 
+    cin Input tahun lahir
+
+    tampilkan nama
+    tampilkan umur
+    tampilkan tahun lahir
+    */ 
+   
+
+      
+    return 0;
+}
