eacf958933bf7d9016f23cf659c482537d1ae530
tianreformis <tianreformis@gmail.com>
2022-04-14 10:16:04 +0800

    Add X MIA

---
+++ 2022-04-1_Input_Output_X_MIA/structure.cpp
@@ -0,0 +1,20 @@
+//header
+#include <iostream> 
+
+//standard space 
+using namespace std;
+
+int main(){ //begin of function
+    //Deklarasi Variabel
+    int nilai, angka, bilangan; //isi var angka = 20, var bilangan = 30
+    //Inisialisasi 
+    nilai = 10;
+    angka = 20;
+    bilangan = 30;
+
+    cout<<angka<<endl;
+    cout<<bilangan<<endl;    
+    cout<<"Kristian Reformis "<<endl; //output
+
+    return 0;
+}//end of function
