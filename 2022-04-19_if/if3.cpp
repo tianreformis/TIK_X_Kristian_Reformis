afcc864aa60feeebf86ed9dc6fbe3ca28ffde149
tianreformis <tianreformis@gmail.com>
2022-04-19 11:53:18 +0800

    tambahkan if kelas X IPS 2

---
+++ 2022-04-19_if/if3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string hari;
+    hari = "Minggu";
+
+    if (hari == "Senin"){
+        cout<<"Putih Abu - Abu";
+    }
+    else if (hari == "Selasa"){
+        cout<<"Seragam Varian";
+    }
+    else if (hari == "Rabu"){
+        cout<<"Batik";
+    }
+    else if (hari == "Kamis"){
+        cout<<"Seragam Varian";
+    }
+    else if (hari == "Jumat"){
+        cout<<"Pramuka";
+    }
+    else {
+        cout<<"Seragam lain";
+    }
+}
