1460b1d38bebaeb785ea5c5e695edbf2ae4f4cfc
tianreformis <tianreformis@gmail.com>
2022-04-26 11:55:08 +0800

    Tambahkan While Kelas X IPS 2

---
+++ 2022-04-26_looping/while2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = 0;
+    while (number++ <10){
+        cout<<" 3 "<<" * "<<number<<" = "<<number * 3<<endl;
+    }
+    cout<<endl<<endl;
+    int number4 = 0;
+    while (number4++ < 10){
+        cout<<" 4 "<<" * "<<number4<<" = "<<number4 * 4<<endl;
+    }
+
+    return 0;
+}
