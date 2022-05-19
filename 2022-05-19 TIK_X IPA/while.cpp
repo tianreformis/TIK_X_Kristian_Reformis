56d9adcbec330979f6ee35439bfc29650aee7f5c
tianreformis <tianreformis@gmail.com>
2022-05-19 14:20:47 +0800

    Tambahkan While 2

---
+++ 2022-05-19 TIK_X IPA/while.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka = 0; //inisialisasi awal
+    while (angka+=2, angka <10) //increment dan batas atas
+    {
+        cout<<angka<<endl;
+    } // perulangan naik
+
+cout<<endl<<endl;
+    int angka2 = 10; //inisialisasi awal
+    while (angka2--, angka2 >1)
+    {
+        cout<<angka2<<endl;
+    }//perulangan turun
+
+
+
+return 0;
+}
+
+/*
+increment = menambah 1 nilai
+decrement = mengurangi 1 nilai
+*/
+
+// Perulangan while
+// Perulangan for
